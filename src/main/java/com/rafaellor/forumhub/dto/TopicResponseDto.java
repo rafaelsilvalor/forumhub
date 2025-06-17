1b58dddbdbd81a6fa7a40741bc5f85740e36bd51
Rafael Silva <rafaelgslor@gmail.com>
2025-06-17 12:01:22 -0300

    feat: Implement TopicRepository and TopicController

--- src/main/java/com/rafaellor/forumhub/dto/TopicResponseDto.java
+++ src/main/java/com/rafaellor/forumhub/dto/TopicResponseDto.java
@@ -1,10 +1,13 @@
+// src/main/java/com/rafaellor/forumhub/dto/TopicResponseDto.java
 package com.rafaellor.forumhub.dto;
 
 import com.rafaellor.forumhub.model.Topic;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import java.time.LocalDateTime;
 
 @Data
+@NoArgsConstructor
 public class TopicResponseDto {
     private Long id;
     private String title;
@@ -14,6 +17,7 @@ public class TopicResponseDto {
     private String author;
     private String course;
 
+    // Constructor to convert Topic entity to DTO
     public TopicResponseDto(Topic topic) {
         this.id = topic.getId();
         this.title = topic.getTitle();
@@ -22,5 +26,16 @@ public class TopicResponseDto {
         this.status = topic.getStatus();
         this.author = topic.getAuthor();
         this.course = topic.getCourse();
+    }
+
+    // Explicit constructor for all fields, to be used for error messages or direct construction
+    public TopicResponseDto(Long id, String title, String message, LocalDateTime creationDate, Boolean status, String author, String course) {
+        this.id = id;
+        this.title = title;
+        this.message = message;
+        this.creationDate = creationDate;
+        this.status = status;
+        this.author = author;
+        this.course = course;
     }
 }
