b7a3059a2f749324a2ac4b2650dada43bdfb51b2
Rafael Silva <rafaelgslor@gmail.com>
2025-06-21 15:27:24 -0300

    feat(auth): Add user registration endpoint

---
+++ src/main/java/com/rafaellor/forumhub/dto/UserCreateDto.java
@@ -0,0 +1,25 @@
+package com.rafaellor.forumhub.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+@Data
+public class UserCreateDto {
+
+    @NotBlank(message = "Name cannot be blank")
+    private String name;
+
+    @NotBlank(message = "Username cannot be blank")
+    @Size(min = 3, message = "Username must have at least 3 characters")
+    private String username;
+
+    @NotBlank(message = "Email cannot be blank")
+    @Email(message = "Invalid email format")
+    private String email;
+
+    @NotBlank(message = "Password cannot be blank")
+    @Size(min = 6, message = "Password must have at least 6 characters")
+    private String password;
+}
